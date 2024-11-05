ed7c26c6474803cd15bfede666bbf87317d74537
suplim2021 <repeat2021@gmail.com>
2024-11-05 12:34:21 +0700

    Added Claude 3.5 Haiku

--- image_tagger_gui.py
+++ image_tagger_gui.py
@@ -170,8 +170,9 @@ class ImageTaggerApp:
         
         self.models = [
             "claude-3-haiku-20240307",
+            "claude-3-5-haiku-latest",
             "claude-3-sonnet-20240229",
-            "claude-3-5-sonnet-20240620"
+            "claude-3-5-sonnet-latest"
         ]
         self.selected_model = tk.StringVar(value=self.models[0])
         
